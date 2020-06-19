9704bdf33585b911a07302a661fb46292278cd13
Daniel <Deadtshot10@gmail.com>
2020-06-19 17:14:23 -0400

    Nuevas Interfaces

--- app/src/main/java/OpenHelper/SQLITE_OpenHelper.java
+++ app/src/main/java/OpenHelper/SQLITE_OpenHelper.java
@@ -2,6 +2,8 @@ package OpenHelper;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.view.View;
@@ -15,7 +17,7 @@ public class SQLITE_OpenHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String query="CREATE TABLE usuarios(ID integer primary key autoincrement," +
+        String query = "CREATE TABLE usuarios(ID integer primary key autoincrement," +
                 "Nombre text,Apellido text,Telefono text,Correo text,Contraseña text)";
         db.execSQL(query);
 
@@ -29,27 +31,38 @@ public class SQLITE_OpenHelper extends SQLiteOpenHelper {
     }
 
     //METODO ABRIR BASE DE DATOS
-    public void abrir(){
+    public void abrir() {
         this.getWritableDatabase();
     }
 
 
     //METODO CERRAR BASE DE DATOS
-    public void cerrar(){
+    public void cerrar() {
         this.close();
     }
 
 
     //METODO QUE INSERTA REGISTRO EN LA TABLA USUARIOS
-    public void insertarReg (String nombre,String apellido,String telefono,String correo,String contraseña){
-        ContentValues valores=new ContentValues();
-        valores.put("Nombre",nombre);
-        valores.put("Apellido",apellido);
-        valores.put("Telefono",telefono);
-        valores.put("Correo",correo);
-        valores.put("Contraseña",contraseña);
-        this.getWritableDatabase().insert("usuarios",null,valores);
+    public void insertarReg(String nombre, String apellido, String telefono, String correo, String contraseña) {
+        ContentValues valores = new ContentValues();
+        valores.put("Nombre", nombre);
+        valores.put("Apellido", apellido);
+        valores.put("Telefono", telefono);
+        valores.put("Correo", correo);
+        valores.put("Contraseña", contraseña);
+        this.getWritableDatabase().insert("usuarios", null, valores);
+
+    }
+
+    //METODO QUE VALIDA SI EL USUARIO EXISTE EN LA BD
+
+    public Cursor consultar_Usu_Contr(String usuario,String contraseña) throws SQLException {
+        Cursor mcursor=null;
+        mcursor=this.getReadableDatabase().query("usuarios",new String[]{"ID",
+                "Nombre","Apellido","Telefono","Correo","Contraseña"},"Correo like '"+usuario+"' " +
+                "and contraseña like '"+contraseña+"'",null,null,null,null);
+
+        return mcursor;
 
     }
 }
-
--- app/src/main/java/com/example/appcompraonline/loginActivity.java
+++ app/src/main/java/com/example/appcompraonline/loginActivity.java
@@ -2,19 +2,25 @@ package com.example.appcompraonline;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.SQLException;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
+
+import OpenHelper.SQLITE_OpenHelper;
 
 public class loginActivity extends AppCompatActivity {
 
     private EditText correo_electronico;
     private EditText contraseña;
-    private Button ingresar;
+    private Button ingresaru;
     private Button registrarse;
     private ImageButton Volver;
+    SQLITE_OpenHelper helper=new SQLITE_OpenHelper(this,"BD1",null,1);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,9 +33,32 @@ public class loginActivity extends AppCompatActivity {
         contraseña = findViewById(R.id.Contraseña_txt);
 
         // Botones
-        ingresar=findViewById(R.id.Boton_ir_ingresar);
+        ingresaru=findViewById(R.id.Boton_Ingresar_U);
         registrarse=findViewById(R.id.Boton_ir_registrarse);
         Volver=findViewById(R.id.Image_btn_regresar);
+
+        ingresaru.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                correo_electronico=findViewById(R.id.Correo_txt);
+                contraseña=findViewById(R.id.Contraseña_txt);
+                try {
+                    Cursor cursor=helper.consultar_Usu_Contr(correo_electronico.getText().toString(),contraseña.getText().toString());
+                    if (cursor.getCount()>0){
+                        Intent i= new Intent(getApplicationContext(),menu_logeadoActivity.class);
+                        startActivity(i);
+                    }else{
+                        Toast.makeText(getApplicationContext(),"Usuario y/o Contraseña incorrectos",Toast.LENGTH_LONG).show();
+                    }
+                    correo_electronico.setText("");
+                    contraseña.setText("");
+                    correo_electronico.findFocus();
+
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 
     public void onclick(View view){
